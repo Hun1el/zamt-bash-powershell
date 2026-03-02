fe73d2acac0a9d442ceda6d70c118a120004d8a5
Anton Solonikov <antonsolonikov@gmail.com>
2026-03-02 20:15:39 +0300

    Pr11. Добавлен powershell файл и модуль.

---
+++ Pr11/PR11.psm1
@@ -0,0 +1,12 @@
+﻿function Get-PSInfo ([string]$fio = "Солоников Антон Сергеевич")
+{
+    $date = Get-Date -Format "MM/dd/yyyy HH:mm:ss"
+    Write-Host "[$date]  $fio"
+}
+
+function Get-PSHeavyPID 
+{
+    Get-Process | Sort-Object -Property PrivateMemorySize -Descending | Select-Object -First 1 | Format-Table Name, Id, PrivateMemorySize -AutoSize
+}
+
+Export-ModuleMember -Function Get-PSInfo, Get-PSHeavyPID
